79823ee1c983740a906186d7fd8ff93b5c5b3ffb
Yash. <thecodeyash@gmail.com>
2023-10-19 15:19:46 +0530

    Background Color Changed ⭐

---
+++ Background Color Changer/app.js
@@ -0,0 +1,17 @@
+const btn = document.getElementById('btn');
+let hex = document.getElementById('hexcode');
+
+function randomColor() {
+  let letter = "0123456789ABCDEF";
+  let color = "#";
+  for(let i=0; i<6; i++) {
+    color += letter[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
+
+btn.addEventListener('click', () => {
+  let bgcolor = randomColor()
+  document.body.style.backgroundColor = bgcolor;
+  hex.innerHTML = bgcolor;
+})
