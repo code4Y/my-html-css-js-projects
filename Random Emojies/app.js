f87951616b46d3513c78e2294e2e72e7e36143b4
code4Y <thecodeyash@gmail.com>
2023-10-25 18:55:57 +0530

    Random Emojies on Mouse Hover Added 🐭

---
+++ Random Emojies/app.js
@@ -0,0 +1,9 @@
+const btn = document.querySelector("#emoji");
+const emojis = [
+  "😆", "😊", "😳", "😁", "😜", "😥", "😂", "😐", "😎",
+  "😷", "😈", "😇", "👽", "😲", "😅", "😖", "😕", "😋",
+];
+
+btn.addEventListener('mouseover', () => {
+  btn.innerHTML = emojis[Math.floor(Math.random() * emojis.length)];
+})
